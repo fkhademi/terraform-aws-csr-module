87fd35c45c3cdeda1c655a42c937ddb8e3f1a37e
Frey Khademi <frey.khademi@gmail.com>
2021-12-02 15:20:19 +0100

    updates

--- security_groups.tf
+++ security_groups.tf
@@ -18,14 +18,14 @@ resource "aws_security_group" "private" {
   }
 }
 
-resource "aws_security_group_rule" "csr_mgmt_ingress" {
+/* resource "aws_security_group_rule" "csr_mgmt_ingress" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = -1
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.public.id
-}
+} */
 
 resource "aws_security_group_rule" "csr_mgmt_egress" {
   type              = "egress"
